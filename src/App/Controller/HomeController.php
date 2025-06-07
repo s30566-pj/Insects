b85e2ee2c0f4a508348616643262484f7a3bef7a
s30566 <s30566@pjwstk.edu.pl>
2025-06-07 10:10:33 +0200

    HomeController and User

---
+++ src/App/Controller/HomeController.php
@@ -0,0 +1,17 @@
+<?php
+namespace App\Controller;
+class HomeController{
+ function index(){
+     echo "
+     <!DOCTYPE html>
+     <html>
+     <head>
+     <meta charset='utf-8'>
+     <title>Home</title>
+     </head>
+     <body>
+     </body>
+     </html>
+     ";
+ }
+}
