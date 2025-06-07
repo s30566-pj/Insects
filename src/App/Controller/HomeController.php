5a2b5edfd387ade88b289d3fd4976d79249d5b6a
s30566 <s30566@pjwstk.edu.pl>
2025-06-07 12:53:22 +0200

    lang en

--- src/App/Controller/HomeController.php
+++ src/App/Controller/HomeController.php
@@ -4,7 +4,7 @@ class HomeController{
  function index(){
      echo "
      <!DOCTYPE html>
-     <html>
+     <html lang='en'>
      <head>
      <meta charset='utf-8'>
      <title>Insects - Home</title>
