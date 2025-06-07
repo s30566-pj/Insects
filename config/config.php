f9641bfd6d817a3801d3b0e550275474dbaed7cc
s30566 <s30566@pjwstk.edu.pl>
2025-06-07 10:10:40 +0200

    config

---
+++ config/config.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    'db' => [
+        'host' => 'localhost',
+        'name' => 'db_name',
+        'user' => 'db_user',
+        'pass' => 'secret',
+        'charset' => 'utf8mb4'
+    ]
+];
