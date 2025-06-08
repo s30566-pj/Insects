3829119cc328face6596931e6d530ef2464a882a
s30566 <s30566@pjwstk.edu.pl>
2025-06-08 17:08:30 +0200

    updated config.php, new array

--- config/config.php
+++ config/config.php
@@ -6,5 +6,10 @@ return [
         'user' => 'db_user',
         'pass' => 'secret',
         'charset' => 'utf8mb4'
+    ],
+    'misc' => [
+        'title' => 'Insects bug tracker',
+        'description' => 'Insects bug tracker',
+        'greeting' => 'Hello world!'
     ]
 ];
