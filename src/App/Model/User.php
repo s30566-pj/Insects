4e55ea29becbed4ae5905e15bdd26c6ec97368ec
s30566 <s30566@pjwstk.edu.pl>
2025-06-08 18:42:14 +0200

    user model class

--- src/App/Model/User.php
+++ src/App/Model/User.php
@@ -1,1 +1,44 @@
 <?php
+namespace App\Model;
+
+class User{
+    private $id;
+    private $name;
+    private $surname;
+    private $email;
+    private $password;
+    private $role;
+    public function getId(){
+        return $this->id;
+    }
+    public function getName(){
+        return $this->name;
+    }
+    public function getSurname(){
+        return $this->surname;
+    }
+    public function getEmail(){
+        return $this->email;
+    }
+    public function getPassword(){
+        return $this->password;
+    }
+    public function getRole(){
+        return $this->role;
+    }
+    public function setName($name){
+        $this->name = $name;
+    }
+    public function setSurname($surname){
+        $this->surname = $surname;
+    }
+    public function setEmail($email){
+        $this->email = $email;
+    }
+    public function setPassword($password){
+        $this->password = $password;
+    }
+    public function setRole($role){
+        $this->role = $role;
+    }
+}
