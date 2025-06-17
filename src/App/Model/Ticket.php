13f021c531be455fa7b01e7035871ce77d125758
s30566 <s30566@pjwstk.edu.pl>
2025-06-17 20:47:41 +0200

    Ticket model update

--- src/App/Model/Ticket.php
+++ src/App/Model/Ticket.php
@@ -8,13 +8,17 @@ class Ticket
     private $title;
     private $description;
     private $status;
+    private $reportedBy;
+    private $resolvedAt;
     private $assigned_to;
 
-    public function __construct($id, $title, $description, $status, $assigned_to){
+    public function __construct($id, $title, $description, $status, $reportedBy, $resolvedAt, $assigned_to){
         $this->id = $id;
         $this->title = $title;
         $this->description = $description;
         $this->status = $status;
+        $this->reportedBy = $reportedBy;
+        $this->resolvedAt = $resolvedAt;
         $this->assigned_to = $assigned_to;
     }
     public function getId(){
@@ -32,6 +36,12 @@ class Ticket
     public function getAssignedTo(){
         return $this->assigned_to;
     }
+    public function getResolvedAt(){
+        return $this->resolvedAt;
+    }
+    public function getReportedBy(){
+        return $this->reportedBy;
+    }
     public function setTitle($title){
         $this->title = $title;
     }
@@ -43,5 +53,11 @@ class Ticket
     }
     public function setAssignedTo($assigned_to){
         $this->assigned_to = $assigned_to;
+    }
+    public function setResolvedAt($resolvedAt){
+        $this->resolvedAt = $resolvedAt;
+    }
+    public function setReportedBy($reportedBy){
+        $this->reportedBy = $reportedBy;
     }
 }
