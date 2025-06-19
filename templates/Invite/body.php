d865373e00d310f306d18965e9766a17df681c9b
s30566 <s30566@pjwstk.edu.pl>
2025-06-19 04:26:00 +0200

    invite styles

--- templates/Invite/body.php
+++ templates/Invite/body.php
@@ -6,22 +6,21 @@
             <a href="/create-ticket"><li>New Issue</li></a>
         </ul>
     </nav>
-
+    <h1>Invite.</h1>
 <main>
-    <h1>Login.</h1>
-    <div class="login-wrapper">
-        <div class="login-box">
-            <div class="loginHead">
+    <div class="invite-wrapper">
+        <div class="invite-box">
+            <div class="inviteHead">
                 <h3>Invite to <?=$org?></h3>
             </div>
-            <div class="login-form">
+            <div class="invite-form">
                 <form method="POST" action="/send-invite">
                     <label for="email">Email</label>
                     <input type="text" name="to_email" placeholder="Email" required>
                     <input type="submit" value="Invite">
                 </form>
             </div>
-            <div class="login-footer">
+            <div class="invite-footer">
                 <p>Want to create a new organization? <a href="/create-organization">Create it!</a></p>
             </div>
         </div>
